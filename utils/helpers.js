524ecf38ae97617d8ef100c3ff12683763b15181
Gortizr <00131725@uca.edu.sv>
2026-09-16 15:18:17 -0600

    :bug: fix: Corregir incidencia.js y helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,9 @@
-const limpiarTexto = (texto) => typeof texto === 'string' ? texto.trim() : texto;
+// Función reutilizable para limpiar espacios en blanco (Uso de trim)
+const limpiarTexto = (texto) => {
+    if (typeof texto === 'string') {
+        return texto.trim();
+    }
+    return texto;
+};
 
 module.exports = { limpiarTexto };
